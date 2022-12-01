29d8c777fbc326f78f28d2e78e586ca80161dd44
Grant Gould <ggould@alum.mit.edu>
2022-12-01 08:55:40 -0500

    Golf the code down to iterators; const-shadow

--- crates/day_01/src/main.rs
+++ crates/day_01/src/main.rs
@@ -1,37 +1,35 @@
 use std::env;
-use std::io::{self, BufRead};
+use std::io::{BufRead, stdin};
 use std::cmp::{min, max};
+
+pub fn stanzas(lines: Vec<String>) -> Vec<Vec<String>> {
+    let mut results : Vec<Vec<String>> = vec![vec![]];
+    for line_text in lines {
+        if line_text.is_empty() {
+            results.push(vec![]);
+        } else {
+            results.last_mut().unwrap().push(line_text.clone());
+        }
+    }
+    results
+}
 
 pub fn main() {
     let args: Vec<String> = env::args().collect();
     let n_elves: usize = {
         if args.len() <= 1 { 1 } else {
             args[1].parse().expect("expected int")}};
-    let mut elf_rations: Vec<Vec<u32>> = vec![vec![0]];
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let line_text = line.unwrap();
-        if line_text.is_empty() {
-            elf_rations.push(vec![]);
-        } else {
-            let ration_cals: u32 = line_text.parse().unwrap();
-            elf_rations.last_mut().expect("no items in list")
-                .push(ration_cals);
-        }
-    }
-    let mut elf_cals: Vec<u32> = vec![0];
-    for elf in elf_rations {
-        let mut total_cals: u32 = 0;
-        for ration in elf {
-            total_cals += ration;
-        }
-        elf_cals.push(total_cals);
-    }
-    elf_cals.sort();
-    let num_to_print = max(0, min(elf_cals.len(), n_elves));
-    let mut top_n_carried_cals = 0;
-    for i in 0..num_to_print {
-        top_n_carried_cals += elf_cals[elf_cals.len() - i - 1];
-    }
-    println!("{}", top_n_carried_cals);
+    let lines = stdin().lock().lines().map(|x| x.unwrap());
+    let stanzas = stanzas(lines.collect());
+    let rations = stanzas.iter().map(
+        |s| s.iter().map(
+            |l| l.parse().unwrap()).collect());
+    let mut ration_totals: Vec<u32> = rations.map(
+        |v: Vec<u32>| v.iter().sum()).collect();
+
+    ration_totals.sort();
+    let ration_totals = ration_totals;
+    let num_to_print = max(0, min(ration_totals.len(), n_elves));
+    let result: u32 = ration_totals[ration_totals.len() - num_to_print ..].iter().sum();
+    println!("{}", result);
 }
