9d5934fd0c621e1d247432366b4791491f5dbc39
Grant Gould <ggould@alum.mit.edu>
2022-12-06 15:50:19 -0500

    solve day 5

--- crates/day_05/src/main.rs
+++ crates/day_05/src/main.rs
@@ -4,44 +4,82 @@ use std::io::{BufRead, stdin};
 type Layout = Vec<Vec<char>>;
 
 fn read_layout(lines: &[String]) -> Layout {
-    let mut layout = vec![];
+    let mut layout: Layout = vec![];
     let mut lines_vec = lines.to_vec();
     lines_vec.reverse();
     for line in lines_vec.iter().skip(1) {
         let num_columns = (line.len() + 1) / 4;
         for col_num in 0..num_columns {
+            if layout.len() < col_num + 1 {
+                layout.push(vec![]);
+            }
             let start_point = col_num * 4;
             let box_str: &str = &line[start_point..(start_point+3)];
-            println!("Saw box {}", box_str);
+            let box_name: char =
+                box_str.chars().nth(1).expect("couldn't get box name");
+            if box_name != ' ' {
+                layout[col_num].push(box_name);
+            }
         }
     }
-    vec![]
+    layout
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 struct Move {
-    count: u8,
-    from: u8,
-    to: u8
+    count: usize,
+    from: usize,
+    to: usize
 }
 
 fn read_moves(lines: &[String]) -> Vec<Move> {
     let move_re = regex::Regex::new(
         r"move (\d+) from (\d+) to (\d+)"
       ).expect("Regex parse error");
-    vec![]
+    let mut result: Vec<Move> = vec![];
+    for line in lines {
+        println!("Parsing line {}", line);
+        let captures = move_re.captures(line).expect("Not a valid move");
+        let this_move: Move = Move{
+            count: captures.get(1).expect("not enough matches")
+                           .as_str().parse().expect("not a valid number"),
+            from: captures.get(2).expect("not enough matches")
+                          .as_str().parse().expect("not a valid number"),
+            to: captures.get(3).expect("not enough matches")
+                        .as_str().parse().expect("not a valid number"),
+        };
+        println!("Move({}, {}, {})", this_move.count, this_move.from, this_move.to);
+        result.push(this_move);
+    }
+    result
 }
 
-fn perform_move(m: Move, layout: &mut Layout) {
+fn perform_multi_move(m: Move, layout: &mut Layout) {
+    for _i in 0..m.count {
+        let payload = layout[m.from - 1].pop().expect("No box available in column");
+        layout[m.to - 1].push(payload);
+        println!("Moved {} from {} to {}", payload, m.from, m.to);
+    }
+}
 
+fn perform_stack_move(m: Move, layout: &mut Layout) {
+    let origin_stack_len = layout[m.from - 1].len();
+    let mut payload = vec![];
+    payload.extend_from_slice(&layout[m.from - 1][origin_stack_len-m.count..]);
+    layout[m.to - 1].extend_from_slice(&payload);
+    layout[(m.from - 1)].truncate(origin_stack_len-m.count);
 }
 
 fn read_top_crates(layout: &Layout) -> String {
-    "".to_string()
+    let mut result: String = "".to_string();
+    for col in layout {
+        result += &col.last().unwrap_or(&'*').to_string();
+    }
+    result
 }
 
 pub fn main() {
-    let _args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     let lines: Vec<String> =
         stdin().lock().lines()
         .map(|x| x.unwrap())
@@ -57,7 +95,14 @@ pub fn main() {
     let moves = read_moves(&moves_text[..]);
     let mut layout = initial_layout;
     for m in moves {
-        perform_move(m, &mut layout);
+        if args[1] == "stacked" {
+            perform_stack_move(m, &mut layout);
+        } else if args[1] == "repeated" {
+            perform_multi_move(m, &mut layout);
+        } else {
+            todo!("not implemented")
+        }
+        println!("{}", read_top_crates(&layout));
     }
     println!("Top crates: {}", read_top_crates(&layout));
 }
